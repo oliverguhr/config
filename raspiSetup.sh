11179f6d5eb6d8cd3959e87e5ed3ec4553c86483
Oliver Guhr <oliver.guhr@gmx.de>
2015-09-27 12:23:38 +0000

    dnvm raspberrypi 2 setup script

---
+++ raspiSetup.sh
@@ -0,0 +1,36 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install htop screen -y
+#ethernet interface
+	
+
+#asp.net and mono
+#tools
+sudo apt-get install unzip curl
+
+#mono framework
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+sudo apt-get update
+sudo apt-get install mono-runtime
+
+#libuv
+
+sudo apt-get install automake libtool
+curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
+cd /usr/local/src/libuv-1.4.2
+sudo sh autogen.sh
+sudo ./configure
+sudo make 
+sudo make install
+sudo rm -rf /usr/local/src/libuv-1.4.2 && cd ~/
+sudo ldconfig
+
+
+#install dnvm
+
+curl -sSL https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.sh | DNX_BRANCH=dev sh && source ~/.dnx/dnvm/dnvm.sh
+
+
+echo Done. Have fun.
+
