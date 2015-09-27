fd039fdbbcb713d2d926cc51e0eb6a3b0f366d03
oliverguhr <p3rfectdark@googlemail.com>
2015-09-27 14:26:44 +0200

    added auto install flags

--- raspiSetup.sh
+++ raspiSetup.sh
@@ -6,17 +6,17 @@ sudo apt-get install htop screen -y
 
 #asp.net and mono
 #tools
-sudo apt-get install unzip curl
+sudo apt-get install unzip curl -y
 
 #mono framework
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 sudo apt-get update
-sudo apt-get install mono-runtime
+sudo apt-get install mono-runtime -y
 
 #libuv
 
-sudo apt-get install automake libtool
+sudo apt-get install automake libtool -y
 curl -sSL https://github.com/libuv/libuv/archive/v1.4.2.tar.gz | sudo tar zxfv - -C /usr/local/src
 cd /usr/local/src/libuv-1.4.2
 sudo sh autogen.sh
