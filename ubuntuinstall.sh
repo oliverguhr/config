17c45c52901611e2e21768556bc6d7a47b15a57c
Oliver Guhr <oliverguhr@users.noreply.github.com>
2022-07-18 15:42:35 +0200

    updated 2022 version

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,9 +1,2 @@
-sudo apt install curl gparted chromium-browser git powertop htop -y
-
-# VS Code with auto update
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-
 sudo apt-get update
-sudo apt-get install code -y
+sudo apt install curl gparted chromium-browser git powertop htop build-essential make cmake  -y
